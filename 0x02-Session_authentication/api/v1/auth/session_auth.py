2b47d011e0251413beb7d0bffed82e69406aa8f5
catherinekimani <catherinekimani882@gmail.com>
2024-02-18 23:43:18 +0300

    Update SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Empty session """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -22,3 +23,17 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """ return user instance based on a cookie value """
+        if request is None:
+            return None
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+
+        if user_id is None:
+            return None
+
+        user = User.get(user_id)
+        return user
